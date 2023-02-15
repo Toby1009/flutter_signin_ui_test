001b5ad7149f750255d985d9f00b0e5cc1d7c573
Toby1009 <st99005912@gmail.com>
2023-02-15 21:02:26 +0800

    Initial commit

--- lib/screen/signin_screen.dart
+++ lib/screen/signin_screen.dart
@@ -5,7 +5,7 @@ import 'package:fluttertoast/fluttertoast.dart'; //吐司的包
 //引入客製化widget
 import 'package:flutter_signin_ui_test/widget/toast_widget.dart';
 import 'package:flutter_signin_ui_test/widget/button_widget.dart';
-import 'package:flutter_signin_ui_test/widget/textfield_widget.dart';
+import 'package:flutter_signin_ui_test/widget/textFormfield_widget.dart';
 
 //routes
 import '../routes/routes.dart';
@@ -81,112 +81,128 @@ class _SignInScreenState extends State<SignInScreen> {
     final width = screenSize.width;
     final heigh = screenSize.height;
 
-    return Scaffold(
-      //防止超出大小
-      body: SafeArea(
-        child: Container(
-          //寬度最大
-          width: double.maxFinite,
-          //高度最大
-          height: double.maxFinite,
-          //內距
-          padding: EdgeInsets.only(left: width / 20, right: width / 20),
-          //垂直widgets
-          child: Column(
-            //主軸
-            mainAxisAlignment: MainAxisAlignment.center,
-            //副軸
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              //裝圖片
-              Container(
-                width: 100,
-                height: 100,
-                alignment: Alignment.center,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/apple.png'),
-                )),
-              ),
-              //留空裝置高度/14
-              SizedBox(
-                height: heigh / 14,
-              ),
-              const Text(
-                'Hello World',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const Text(
-                'We are very missing you',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.grey,
-                ),
-              ),
-              SizedBox(
-                height: heigh / 20,
-              ),
-              TextFieldWidget(
-                textEditingController: emailController,
-                hintText: "Email",
-                iconData: Icons.person,
-              ),
-              SizedBox(
-                height: heigh / 90,
-              ),
-              TextFieldWidget(
-                textEditingController: passwordController,
-                hintText: "Password",
-                obscureText: true,
-                iconData: Icons.key,
-              ),
-              SizedBox(
-                height: heigh / 90,
-              ),
-              //登入按鈕
-              InkWell(
-                onTap: (){
-                  if (haveEmailPassword()) {
-                    _notEmptyEmailPassword();
-                  } else {
-                    _emptyEmailPassword();
-                  }
-                },
-                  child: const ButtonWidget(text: "Sign In")
-              ),
-              SizedBox(
-                height: heigh / 90,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Not a member?",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, Routes.registerScreen);
-                    },
-                    child: const Text(
-                      "Register now",
-                      style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
+    Widget imageWidget(){
+      return Container(
+        width: 100,
+        height: 100,
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/apple.png'),
+            )),
+      );
+    }
+    Widget emailField(){
+      return  TextFormFieldWidget(
+        textEditingController: emailController,
+        hintText: "Email",
+        iconData: Icons.person,
+      );
+    }
+
+    Widget passwordField(){
+      return TextFormFieldWidget(
+        textEditingController: passwordController,
+        hintText: "Password",
+        obscureText: true,
+        iconData: Icons.key,
+      );
+    }
+
+    Widget signInButton(){
+      return InkWell(
+          onTap: (){
+            if (haveEmailPassword()) {
+              _notEmptyEmailPassword();
+            } else {
+              _emptyEmailPassword();
+            }
+          },
+          child: const ButtonWidget(text: "Sign In")
+      );
+    }
+
+    Widget registerButton(){
+      return TextButton(
+        onPressed: () {
+          Navigator.pushNamed(context, Routes.registerScreen);
+        },
+        child: const Text(
+          "Register now",
+          style: TextStyle(
+            color: Colors.blue,
           ),
         ),
-      ),
+      );
+    }
+
+    Widget signInForm(){
+      return Form(
+        child: SafeArea(
+          child: Container(
+            //寬度最大
+            width: double.maxFinite,
+            //高度最大
+            height: double.maxFinite,
+            //內距
+            padding: EdgeInsets.only(left: width / 20, right: width / 20),
+            //垂直widgets
+            child: Column(
+              //主軸
+              mainAxisAlignment: MainAxisAlignment.center,
+              //副軸
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                //裝圖片
+                imageWidget(),
+                //留空裝置高度/14
+                SizedBox(
+                  height: heigh / 14,
+                ),
+                const Text(
+                  'Hello World',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  'We are very missing you',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(height: heigh / 20,),
+                emailField(),
+                SizedBox(height: heigh / 90,),
+                passwordField(),
+                SizedBox(height: heigh / 90,),
+                //登入按鈕
+                signInButton(),
+                SizedBox(height: heigh / 90,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Not a member?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    registerButton(),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: signInForm(),
     );
   }
 }
