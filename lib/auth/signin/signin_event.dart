92473c99decfe567bf1fe47a730a65fd81e68675
Toby1009 <st99005912@gmail.com>
2023-02-16 13:35:11 +0800

    202302160135

---
+++ lib/auth/signin/signin_event.dart
@@ -0,0 +1,16 @@
+abstract class SignInEvent{}
+
+class SignInEmailChange extends SignInEvent{
+  final String? email;
+
+  SignInEmailChange({this.email});
+}
+
+class SignInPasswordChange extends SignInEvent{
+  final String? password;
+  SignInPasswordChange({this.password});
+}
+
+class SignInSubmitted extends SignInEvent{
+
+}
